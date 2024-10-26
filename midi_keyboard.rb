eaf83e29da0f030842874ade4d5187ddd7b91686
Luc Yriarte <lyriarte@gmail.com>
2024-10-27 00:30:33 +0200

    play notes while key down

--- midi_keyboard.rb
+++ midi_keyboard.rb
@@ -1,6 +1,20 @@
 #  ---- ----  ----  ---- midi keyboard input
 
 
+# ---- midi chord
+
+synth_nodes = []
+
+define :play_midi_note do | nt |
+  synth_nodes[nt] = play nt, sustain: 8, release: 1
+end
+
+define :stop_midi_note do | nt |
+  control synth_nodes[nt], amp: 0
+  synth_nodes[nt].kill
+  synth_nodes[nt] = nil
+end
+
 
 
 # ---- midi event capture
@@ -9,11 +23,14 @@ live_loop :midi_note_on do
   use_real_time
   # sync keydown event
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_on"
-  play nt
+  # play the note now and store the synth node
+  play_midi_note nt
 end
 
 live_loop :midi_note_off do
   use_real_time
   # sync keyup event
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_off"
+  # get the synth node and stop playing the note
+  stop_midi_note nt
 end
