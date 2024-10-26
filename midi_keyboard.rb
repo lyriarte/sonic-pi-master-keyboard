5912f6d11cba8f890b07a297bbc5dfe0d1f577bb
Luc Yriarte <lyriarte@gmail.com>
2024-10-26 23:31:22 +0200

    use real time for processing events

--- midi_keyboard.rb
+++ midi_keyboard.rb
@@ -1,12 +1,19 @@
 #  ---- ----  ----  ---- midi keyboard input
 
+
+
+
 # ---- midi event capture
 
 live_loop :midi_note_on do
+  use_real_time
+  # sync keydown event
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_on"
   play nt
 end
 
 live_loop :midi_note_off do
+  use_real_time
+  # sync keyup event
   nt, vl = sync "/midi:midi_through_port-0:0:1/note_off"
 end
